a9914428fec492a92a44b86307eb338e1ce81a51
Víctor Martínez <victorandresmtz@gmail.com>
2020-03-03 16:07:21 -0600

    feat(app): add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarModule, WavesModule, ButtonsModule, IconsModule } from 'ng-uikit-pro-standard';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NavbarModule, WavesModule, ButtonsModule, IconsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
